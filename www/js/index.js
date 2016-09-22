d1f3c0c88b541680de411842e6d8c025a02b6eea
TecDoGt <ddelaguila@tecdogt.com>
2016-09-22 16:24:00 -0600

    ver 2.0.3 background Send GPS prosses

--- www/js/index.js
+++ www/js/index.js
@@ -3326,3 +3326,20 @@ $.mobile.document
     
         $(".ui-body-a").addClass("ui-body-b").removeClass("ui-body-a");
     });
+
+document.addEventListener('deviceready', function () {
+    // Android customization
+    cordova.plugins.backgroundMode.setDefaults({ text:'Doing heavy tasks.'});
+    // Enable background mode
+    cordova.plugins.backgroundMode.enable();
+
+    // Called when background mode has been activated
+    cordova.plugins.backgroundMode.onactivate = function () {
+        setTimeout(function () {
+            // Modify the currently displayed notification
+            cordova.plugins.backgroundMode.configure({
+                text:'Running in background for more than 5s now.'
+            });
+        }, 5000);
+    }
+}, false);
